24b743a30093dd9c297de3774557512b7e2297be
IAAN SECURITY <32310877+L0WK3Y-IAAN@users.noreply.github.com>
2024-10-23 16:02:15 -0400

    Update disabler.bat

--- disabler.bat
+++ disabler.bat
@@ -1,25 +1,46 @@
-@set defenderremoverver=12.8
-@setlocal DisableDelayedExpansion
 @echo off
-pushd "%CD%"
-CD /D "%~dp0"
+:: Elevate privileges if not already running as admin
+:: Check for admin rights
+net session >nul 2>&1
+if %errorLevel% neq 0 (
+    echo This script requires administrative privileges.
+    pause
+    exit /b
+)
 
-:: Skip the choice section and go straight to disabling all security mitigations
-goto disablemitigations
+echo.
+echo ---- Disabling Windows Defender ----
+echo.
 
-:--------------------------------------
+:: Disable Windows Defender Antivirus Service
+sc stop WinDefend
+sc config WinDefend start= disabled
+echo Windows Defender service stopped and disabled.
 
-:disablemitigations
-CLS
-bcdedit /set hypervisorlaunchtype off
+:: Disable SecurityHealthService
+sc stop SecurityHealthService
+sc config SecurityHealthService start= disabled
+echo SecurityHealthService stopped and disabled.
 
-CLS
-echo Disabling Security Mitigations...
-:: Registry Remove of Windows Defender
-FOR /R %%f IN (Remove_SecurityComp\*.reg) DO PowerRun.exe regedit.exe /s "%%f"
-CLS
-echo Your PC will reboot in 10 seconds..
-timeout 10
-shutdown /r /f /t 0
+:: Disable Windows Defender scheduled tasks
+schtasks /Change /TN "Microsoft\Windows\Windows Defender\Windows Defender Cache Maintenance" /Disable
+schtasks /Change /TN "Microsoft\Windows\Windows Defender\Windows Defender Cleanup" /Disable
+schtasks /Change /TN "Microsoft\Windows\Windows Defender\Windows Defender Scheduled Scan" /Disable
+schtasks /Change /TN "Microsoft\Windows\Windows Defender\Windows Defender Verification" /Disable
+echo Windows Defender tasks disabled.
+
+:: Disable Windows Defender real-time protection using Registry
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows Defender" /v DisableAntiSpyware /t REG_DWORD /d 1 /f
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows Defender\Real-Time Protection" /v DisableRealtimeMonitoring /t REG_DWORD /d 1 /f
+echo Disabled real-time protection in registry.
+
+:: Disable Tamper Protection (only on newer Windows versions, not always effective)
+reg add "HKLM\SOFTWARE\Microsoft\Windows Defender\Features" /v TamperProtection /t REG_DWORD /d 0 /f
+echo Disabled Tamper Protection.
+
+echo.
+echo ---- Windows Defender should now be disabled. ----
+echo A reboot may be required for all changes to take effect.
+echo.
+pause
 exit
-:--------------------------------------
