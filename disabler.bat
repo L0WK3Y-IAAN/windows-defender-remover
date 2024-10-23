d928fc7e77e6366c4b1e936ee91cbf8db093d841
IAAN SECURITY <32310877+L0WK3Y-IAAN@users.noreply.github.com>
2024-10-23 16:27:02 -0400

    Update disabler.bat

--- disabler.bat
+++ disabler.bat
@@ -1,6 +1,5 @@
 @echo off
 :: Elevate privileges if not already running as admin
-:: Check for admin rights
 net session >nul 2>&1
 if %errorLevel% neq 0 (
     echo This script requires administrative privileges.
@@ -8,19 +7,16 @@ if %errorLevel% neq 0 (
     exit /b
 )
 
-echo.
-echo ---- Disabling Windows Defender ----
-echo.
+:: Hide Defender icon from the taskbar and stop the SecurityHealthService
+sc stop SecurityHealthService
+sc config SecurityHealthService start= disabled
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /v "SecurityHealth" /f
+echo Removed Windows Defender icon from taskbar.
 
 :: Disable Windows Defender Antivirus Service
 sc stop WinDefend
 sc config WinDefend start= disabled
 echo Windows Defender service stopped and disabled.
-
-:: Disable SecurityHealthService
-sc stop SecurityHealthService
-sc config SecurityHealthService start= disabled
-echo SecurityHealthService stopped and disabled.
 
 :: Disable Windows Defender scheduled tasks
 schtasks /Change /TN "Microsoft\Windows\Windows Defender\Windows Defender Cache Maintenance" /Disable
@@ -38,9 +34,12 @@ echo Disabled real-time protection in registry.
 reg add "HKLM\SOFTWARE\Microsoft\Windows Defender\Features" /v TamperProtection /t REG_DWORD /d 0 /f
 echo Disabled Tamper Protection.
 
+:: Remove Windows Defender from Start Search
+PowerShell -Command "Get-AppxPackage *Microsoft.Windows.SecHealthUI* | Remove-AppxPackage"
+echo Removed Windows Defender from Start menu.
+
 echo.
 echo ---- Windows Defender should now be disabled. ----
 echo A reboot may be required for all changes to take effect.
 echo.
-pause
 exit
